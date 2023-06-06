6953257cbc5db01a6a6b5b19913b2352ae90bb00
GagarisnSN <gagarin.sn@yandex.ru>
2023-06-06 12:37:29 +0500

    feat: get done services

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
 
@@ -11,19 +11,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/films")
 public class FilmController {
+    private final FilmService filmService;
 
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
 
     @PostMapping
     public Film create(@RequestBody Film film) {
+        filmService.createFilm(film);
         return film;
     }
 
     @PutMapping
     public Film update(@RequestBody Film film) {
+        filmService.updateFilm(film);
         return film;
     }
 
     @GetMapping
     public List<Film> findAll() {
+        return filmService.findAll();
     }
 }
