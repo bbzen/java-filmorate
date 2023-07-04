980d6c7c047775c7a4d11aa043ea4f33a1a251b0
GagarinSN <gagarin.sn@yandex.ru>
2023-07-04 19:15:26 +0500

    feat: add several methods in UserDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.filmorate.storage.user;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
@@ -29,6 +31,7 @@ public class UserDbStorage implements UserStorage {
         Map<String, Object> params = Map.of("email", user.getEmail(), "login", user.getLogin(), "user_name", user.getName(), "birthday", user.getBirthday());
         Number id = simpleJdbcInsert.executeAndReturnKey(params);
         user.setId(id.intValue());
+        log.debug("User " + user.getLogin() + " has been added.");
         return user;
     }
 
@@ -36,18 +39,25 @@ public class UserDbStorage implements UserStorage {
     public User updateUser(User user) {
         String sql = "update users set email = ?, login = ?, user_name = ?, birthday = ? where user_id = ?";
         jdbcTemplate.update(sql, user.getEmail(), user.getLogin(), user.getName(), user.getBirthday(), user.getId());
-        log.debug("User" + user + "was updated.");
+        log.debug("User " + user.getLogin() + " was updated.");
         return user;
     }
 
     @Override
     public void removeUser(User user) {
-
+        String sql = "delete from users where user_id = ?";
+        jdbcTemplate.update(sql, user.getId());
+        log.debug("User " + user.getLogin() + " has been removed.");
     }
 
     @Override
     public User getUserById(int id) {
-        return null;
+        String sql = "select * from users where user_id = ?";
+        List<User> users = jdbcTemplate.query(sql, userRowMapper(), id);
+        if (users.size() != 1) {
+            throw new UserNotFoundException("User with id " + id + " not found.");
+        }
+        return users.get(0);
     }
 
     @Override
@@ -58,5 +68,15 @@ public class UserDbStorage implements UserStorage {
     @Override
     public boolean containsUser(int id) {
         return false;
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return (rs, rowNum) -> new User(
+                rs.getInt("user_id"),
+                rs.getString("email"),
+                rs.getString("login"),
+                rs.getString("user_name"),
+                rs.getDate("birthday").toLocalDate()
+        );
     }
 }
