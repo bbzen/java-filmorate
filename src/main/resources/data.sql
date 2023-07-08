522fd6a717465c04a84474c3c3f1c03b25e915f7
GagarinSN <gagarin.sn@yandex.ru>
2023-07-08 10:30:25 +0500

    feat: connect h2 db finally

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into mpa(mpa_name) values ('G');
+insert into mpa(mpa_name) values ('PG');
+insert into mpa(mpa_name) values ('PG-13');
+insert into mpa(mpa_name) values ('R');
+insert into mpa(mpa_name) values ('NC-17');
+
+insert into genres(genre_name) values ('Комедия');
+insert into genres(genre_name) values ('Драма');
+insert into genres(genre_name) values ('Мультфильм');
+insert into genres(genre_name) values ('Триллер');
+insert into genres(genre_name) values ('Документальный');
+insert into genres(genre_name) values ('Боевик');
