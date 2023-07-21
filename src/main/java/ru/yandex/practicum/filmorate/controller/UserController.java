57cd1c7578a55188da2e66af9737eb0745a23670
Nikolai <gardemarinin5@yandex.ru>
2023-07-22 00:24:55 +0500

    final fix

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -8,9 +8,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 @Slf4j
 @RestController
